828bb33ad93deb61a994116cba2e011b67c673a8
Philip Sotirov <me@philipsotirov.com>
2018-01-14 17:01:33 +0200

    Add PropTypes: Delete button

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Delete = props => (
   <button onClick={() => props.handleDelete(props.id)}>Delete</button>
 )
+
+Delete.propTypes = {
+  id: PropTypes.number.isRequired,
+  handleDelete: PropTypes.func.isRequired
+}
 
 export default Delete
